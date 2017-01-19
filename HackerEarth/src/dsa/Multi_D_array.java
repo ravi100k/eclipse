cf71969ee8cbef884c6f05ce3a05bb067eb30b8f
ravi kumar <ravi100k>
2017-01-19 17:20:07 +0530

    2d array commit

--- HackerEarth/src/dsa/Multi_D_array.java
+++ HackerEarth/src/dsa/Multi_D_array.java
@@ -7,16 +7,23 @@ public class Multi_D_array {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc= new Scanner(System.in);
-		int row = 0,coll = 0;
-		row=sc.nextInt();
-		coll=sc.nextInt();
-		int[][] Array = new int[row][coll];
+		int row;
+		int coll;
 		
-		for(int i=0;i<row;i++){
-			for(int j=0;j<coll;j++){
-				System.out.println(i+" "+j);
+		row = sc.nextInt();
+		coll = sc.nextInt();
+		int[][] Array = new int[row][coll];
+
+
+		for (row= 0; row < Array.length; row++) {
+			for(coll = 0; coll < Array[row].length; coll++) {
+			    //System.out.println(i+j);
+			    Array[row][coll] = sc.nextInt(); //Storing input value here    
+			    System.out.println((Array[row][coll]));//Output the input value
 			}
-		}		
+			System.out.println();
+		}
+		
 
 	}
 
